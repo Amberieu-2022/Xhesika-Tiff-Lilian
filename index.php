564fd1c400721c3f7a20f1a25f47b1f868621fa3
XhesikaMilaqi <jessy.99milaqi@gmail.com>
2022-03-18 11:57:26 +0100

    Form index user - Form login - Form gite - Header /footer responsive

--- index.php
+++ index.php
@@ -3,19 +3,29 @@
 require_once './templates/header.php';
 
 ?>
+<main>
+    <h1 class="titre-h1">Bienvenue</h1>
+    <!-- FORMULAIRE UTILISATEUR : TROUVER VOTRE GITE  -->
+    <section class="form-page-client">
+        <h2 class="titre-form-client">TROUVEZ VOTRE GÎTE</h2>
+        <form action="#" method="GET">
 
-<h1 class="titre-h1">Bienvenue</h1>
-<section class="form-page-client">
-    <h2 class="titre-form-client">TROUVEZ VOTRE GÎTE</h2>
-    <form action="#" method="GET">
-        <label class="">DESTINATION</label>
-        <input type="text" class=""> <br>
-        <label for="">ARRIVÉE</label> <input type="text" value="">
-        <label for=""> DEPART</label> <input type="text" value="">
+            <div class="location">
+                <label class="label-client">DESTINATION</label>
+                <input type="text" placeholder=" Ecrivez votre destination" class="input-local">
+            </div>
 
-        <div class="first-flex">
-                <label class="label-no"> Nombre de couchages </label>
-                <select name="nb-personnes" id="">
+            <div class="location">
+                <label for="" class="label-client">ARRIVÉE</label>
+                <input type="date" value="" class="date-placeholder">
+                <label for="" class="label-client"> DEPART</label>
+                <input type="date" value="" class="date-placeholder">
+            </div>
+
+
+            <div class="first-flex">
+                <label class="label-client"> Nombre de couchages </label>
+                <select name="nb-personnes" id="" class="nb-personnes">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -30,10 +40,10 @@ require_once './templates/header.php';
                     <option value="12">12</option>
                     <option value="13">13</option>
                     <option value="14">14</option>
-                    <option value="15">15</option> 
+                    <option value="15">15</option>
                 </select>
-                <label class="label-no"> Nombre de salle de bain </label>
-                <select name="nb-bathroom" id="">
+                <label class="label-client"> Nombre de salle de bain </label>
+                <select name="nb-bathroom" id="" class="nb-personnes">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -42,8 +52,9 @@ require_once './templates/header.php';
                     <option value="6">6</option>
                 </select>
 
-                <label class="label-no"> Nombre de chambres </label>
-                <select name="nb-room" id="">
+                <label class="label-client"> Nombre de chambres </label>
+
+                <select name="nb-room" id="" class="nb-personnes">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -56,14 +67,43 @@ require_once './templates/header.php';
                     <option value="10">10</option>
                 </select>
 
-        </div>
-        
+            </div>
+            <br>
+            <label for="type" class="label-client">Type de logement</label> <br>
+            <div class="first-flex flex-2">
+                <input type="checkbox" name="gite[]" value="" class="input-option"><label class="label-client"> Chambre </label>
+                <input type="checkbox" name="gite[]" value="" class="input-option"><label class="label-client"> Appartement </label>
+                <input type="checkbox" name="gite[]" value="" class="input-option"><label class="label-client"> Maison </label>
+                <input type="checkbox" name="gite[]" value="" class="input-option"><label class="label-client"> Villa </label>
+            </div>
+            <br>
+            <label for="option" class="label-client"> Option(s) </label> <br>
+            <div class="first-flex flex-2">
+                <input type="checkbox" name="gite[]" value="" class="input-option"><label class="label-client"> Piscine </label>
+                <input type="checkbox" name="gite[]" value="" class="input-option"><label class="label-client"> Jardin </label>
+                <input type="checkbox" name="gite[]" value="" class="input-option"><label class="label-client"> Parking </label>
+                <input type="checkbox" name="gite[]" value="" class="input-option"><label class="label-client"> Animaux acceptée </label>
+            </div>
+            <br>
+
+            <button class="btn-util">Rechercher </button>
 
 
-        
-    </form>
-</section>
+        </form>
+    </section>
 
+    <h2 class="titre-past-form">NOUS AVONS TROUVÉ LES GÎTES SUIVANTS POUR VOUS</h2>
+    <aside class="form-filter">
+        <form action="#" method="GET">
+            <label for="">Filtrer par </label> <br>
+            <label for="">Categorie(s)</label>
+            <div class="categorie">
+                
+            </div>
+        </form>
+    </aside>
+    
+</main>
 <?php
 
 require_once './templates/footer.php';
